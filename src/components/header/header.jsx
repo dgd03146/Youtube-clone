2578a9967c0607f480e31f85d83f8ea4ee4145f8
jung <ibory1220@naver.com>
2021-11-30 14:29:27 +0900

    Search function implement

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './header.module.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import MenuIcon from '@mui/icons-material/Menu';
@@ -8,7 +8,21 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import SearchIcon from '@mui/icons-material/Search';
 import avatar from './../../images/avatar.jpg';
 
-function Header() {
+const Header = ({ onSearch }) => {
+  const inputRef = useRef();
+  const handleSearch = () => {
+    const query = inputRef.current.value;
+    onSearch(query);
+  };
+
+  const onClick = () => {
+    handleSearch();
+  };
+  const onKeyPress = event => {
+    if (event.key === 'Enter') {
+      handleSearch();
+    }
+  };
   return (
     <nav className={styles.header}>
       <div className={styles.bar_left}>
@@ -21,11 +35,15 @@ function Header() {
       </div>
       <div className={styles.bar_search}>
         <input
+          ref={inputRef}
           className={styles.input}
-          type="text"
+          type="search"
           placeholder="Enter the keywords"
+          onKeyPress={onKeyPress}
         />
-        <SearchIcon className={styles.inputBtn} />
+        <button className={styles.inputBtn} onClick={onClick}>
+          <SearchIcon className={styles.searchIcon} />
+        </button>
       </div>
       <div className={styles.bar_right}>
         <VideoCallIcon className={styles.videocallIcon} />
@@ -37,6 +55,6 @@ function Header() {
       </div>
     </nav>
   );
-}
+};
 
 export default Header;
--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -6,9 +6,23 @@ import styles from './main.module.css';
 
 function Main() {
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCPbzL0dj-lklB6dc5L3qQ3Bl-E-_TkM8o`,
+      requestOptions
+    )
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(() => {
-    let requestOptions = {
+    const requestOptions = {
       method: 'GET',
       redirect: 'follow',
     };
@@ -18,13 +32,16 @@ function Main() {
       requestOptions
     )
       .then(response => response.json())
-      .then(result => setVideos(result.items))
+      .then(result =>
+        result.items.map(item => ({ ...item, id: item.id.videoId }))
+      )
+      .then(items => setVideos(items))
       .catch(error => console.log('error', error));
   }, []);
 
   return (
     <div className={styles.mainPage}>
-      <Header />
+      <Header onSearch={search} />
       <div className={styles.content}>
         <Sidebar />
         <VideoList videos={videos} />
