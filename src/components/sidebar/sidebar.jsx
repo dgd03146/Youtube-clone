c40414e6240d591937bf5a3e08b2cf7c192e001b
jung <ibory1220@naver.com>
2021-12-06 18:28:39 +0900

    Modify responsive

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -12,10 +12,6 @@ import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
 import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 
 function Sidebar() {
-  // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-  let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
   return (
     <div className={styles.sideBar}>
       <SidebarRow selected Icon={HomeIcon} title="Home" />
